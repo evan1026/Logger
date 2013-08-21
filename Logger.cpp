cce293fa3929ccf6d4e3d1333d8544819827bd6c
Evan Allan <evanallan1026@gmail.com>
2013-08-21 01:08:20 -0400

    Turns out rewriting doesn't work

--- Logger.cpp
+++ Logger.cpp
@@ -1,22 +1,26 @@
 #include "Logger.hpp"
 
+Logger::Logger(){}
+
 void Logger::log(int type, std::string message, bool color){
+    std::string output = "";
     switch (type) {
         case Logger::LOG_INFO:
-            std::cout << "[INFO] ";
+            output += "[INFO] ";
             break;
         case Logger::LOG_WARNING:
-            std::cout << (color ? "\e[33m[WARNING] " : "[WARNING] ");
+            output += (color ? "\e[33m[WARNING] " : "[WARNING] ");
             break;
         case Logger::LOG_ERROR:
-            std::cout << (color ?  "\e[31m[ERROR] " : "[ERROR] ");
+            output += (color ?  "\e[31m[ERROR] " : "[ERROR] ");
             break;
         default:
             Logger::log(LOG_ERROR, "Log type not found");
             break;
     }
-    std::cout << message << (color ? "\e[m" : "") << std::endl;
-    prevLength = message.length() + (color ? 5 : 1);
+    output += message + (color ? "\e[m" : "");
+
+    std::cout << output << std::endl;
 }
 
 void Logger::log(int type, std::string message){
@@ -25,25 +29,4 @@ void Logger::log(int type, std::string message){
 
 void Logger::log(std::string message){
     Logger::log(Logger::LOG_INFO, message);
-}
-
-void Logger::logrw(int type, std::string message, bool color){
-    Logger::eraseLast();
-    Logger::log(type, message, color);
-}
-
-void Logger::logrw(int type, std::string message){
-    Logger::logrw(type, message, false);
-}
-
-void Logger::logrw(std::string message){
-    Logger::logrw(Logger::LOG_INFO, message);
-}
-
-void Logger::eraseLast(){
-    for (int i = 0; i < Logger::prevLength; i++){
-        std::cout << "\b";
-    }
-    std::cout.flush();
-    prevLength = 0;
 }
--- Logger.hpp
+++ Logger.hpp
@@ -9,17 +9,11 @@ struct Logger {
     static const int LOG_WARNING = 1;
     static const int LOG_ERROR   = 2;
 
-    private int prevLength = 0;
-
-    static void log(int type, std::string message, bool color);
-    static void log(int type, std::string message);
-    static void log(std::string message);
+    Logger();
     
-    static void logrw(int type, std::string message, bool color);
-    static void logrw(int type, std::string message);
-    static void logrw(std::string message);
-
-    static void eraseLast();
+    void log(int type, std::string message, bool color);
+    void log(int type, std::string message);
+    void log(std::string message);
 };
 
 #endif
